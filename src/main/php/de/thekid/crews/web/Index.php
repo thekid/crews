1ad18a3b2bb5999058829ce9d660c9644ad92e53
Timm Friebe <thekid@thekid.de>
2023-12-14 22:34:20 +0100

    Add create dialog

--- src/main/php/de/thekid/crews/web/Index.php
+++ src/main/php/de/thekid/crews/web/Index.php
@@ -1,7 +1,8 @@
 <?php namespace de\thekid\crews\web;
 
 use com\mongodb\{Database, Collection, Document};
-use web\frontend\{Handler, Get, View};
+use util\Date;
+use web\frontend\{Handler, Get, Post, Param, View};
 
 #[Handler('/')]
 class Index {
@@ -18,5 +19,36 @@ class Index {
       ['$limit' => 20],
     ]);
     return View::named('index')->with(['groups' => $groups->all()]);
+  }
+
+  #[Get('/create')]
+  public function show() {
+    return View::named('index#create');
+  }
+
+  #[Delete('/create')]
+  public function hide() {
+    return View::empty()->status(202);
+  }
+
+  #[Post('/create')]
+  public function create(#[Param] $name, #[Param] $description) {
+    $name= trim($name);
+
+    // Verify name is unique, return "Multiple Choices" status code
+    if ($group= $this->groups->find(['name' => $name])->first()) {
+      return View::named('index#form')
+        ->status(300)
+        ->with(['name' => $name, 'description' => $description, 'taken' => $group])
+      ;
+    }
+
+    // Create, then trigger redirect
+    $insert= $this->groups->insert(new Document([
+      'name'        => $name,
+      'description' => $description,
+      'created'     => Date::now(),
+    ]));
+    return View::empty()->header('HX-Redirect', "/group/{$insert->id()}");
   }
 }
